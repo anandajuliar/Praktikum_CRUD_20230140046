890b438e4a555bda77f7f248efcf5ab64306553c
anandajuliar <anandajulia001@gmail.com>
2026-03-03 14:40:51 +0700

    membuat javaclass (Vlidation util) dipackege util

--- src/main/java/com/deploy/praktikum1/util/ValidationUtil.java
+++ src/main/java/com/deploy/praktikum1/util/ValidationUtil.java
@@ -1,10 +1,25 @@
 package com.deploy.praktikum1.util;
 
-import com.deploy.praktikum1.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Repository
-public interface ValidationUtil extends JpaRepository<User, String> {
+import java.util.Set;
+
+@Component
+@AllArgsConstructor
+public class ValidationUtil {
+
+    @Autowired
+    private Validator validator;
+
+    public void validate(Object request) {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(request);
+        if (constraintViolations.size() != 0) {
+            throw new ConstraintViolationException(constraintViolations);
+        }
+    }
 }
-
